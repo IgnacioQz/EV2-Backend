b425f216ad83c08e7435ae8fab351dbef56141cb
IgnacioQz <nacho.quierozepeda@gmail.com>
2025-10-15 16:48:12 -0300

    vistas creadas

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def categoria(request):
+    return render(request, 'categoria.html')
+
+def detalleProducto(request):
+    return render(request, 'detalle.html')
