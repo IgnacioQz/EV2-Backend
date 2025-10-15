ba0c77a85e5abe725cb101721b353b66d21fb200
IgnacioQz <nacho.quierozepeda@gmail.com>
2025-10-15 16:29:54 -0300

    nodelo actualizado

--- core/models.py
+++ core/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Producto(models.Model):
     nombreProducto  = models.CharField(max_length=100)
+    categoria       = models.CharField(max_length=50, null = True)
     precio          = models.FloatField()
     stock           = models.IntegerField()
     descripcion     = models.TextField()
