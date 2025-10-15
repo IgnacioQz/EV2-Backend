f94bdf0b80fa661b0c4069c6198323b93f892379
IgnacioQz <nacho.quierozepeda@gmail.com>
2025-10-15 16:50:20 -0300

    vistas creadas 2

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/', views.home, name='home'),
+    path('categoria/', views.categoria, name='categoria'),
+    path('detalle/', views.detalleProducto, name='detalleProducto'),
+]
