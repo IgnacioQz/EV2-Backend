4c8772a8707b9fbc9c5db0dd858d5898f0560d87
IgnacioQz <nacho.quierozepeda@gmail.com>
2025-10-16 23:32:24 -0300

    home html muestra productos

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('categoria/', views.categoria, name='categoria'),
     path('detalle/', views.detalleProducto, name='detalleProducto'),
+    path('producto/<int:id>/', views.producto_detalle, name='producto_detalle'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
+from .models import Producto
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    context = {}
+
+    productos = Producto.objects.all()
+    context['productos'] = productos
+
+    return render(request, 'home.html',context)
+
+def producto_detalle(request,id):
+    
+    context = {}
+    
+    productos = Producto.objects.get(id=id)    
+    context["productos"] = productos
+    return render(request,"producto_detalle.html", context)
 
 def categoria(request):
     return render(request, 'categoria.html')
